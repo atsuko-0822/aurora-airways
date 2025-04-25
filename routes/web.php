8c7191ef6e1daba7d6efb2b858e56d209fb64efa
Atsuko <atk.822@gmail.com>
2025-04-25 20:59:16 +0900

    [UI/user_registration] - create user registration page Additions: -added user_registration.blade.php -added route to web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/user_registration', function () {
+    return view('user_registration');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
