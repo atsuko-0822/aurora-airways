26df7e953c50bb738b308cf3190286ff696637a2
Atsuko <atk.822@gmail.com>
2025-04-28 19:45:06 +0900

    [UI/user-login] -add user_login.blade.php  Additions: -user_login.blade.php -put a logo image in images
    Modified
    -registration.css
    -app.js
    -app.scss
    -background.blade.php
    -web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/user_registration', function () {
     return view('user_registration');
 });
 
+Route::get('/user_login', function () {
+    return view('user_login');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
