6b52f09c050de279a71da762670c695633d47702
Kodai Sato <satkou115@gmail.com>
2025-04-30 21:02:32 +0900

    web.php

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/test', function () {
     return view('test');
 });
 
-=======
 Route::get('/user_registration', function () {
     return view('user_registration');
 });
@@ -19,7 +17,6 @@ Route::get('/user_registration', function () {
 Route::get('/user_login', function () {
     return view('user_login');
 });
->>>>>>> 8a2d01617e9c6e36b3f6f0dacb2bab66078184c8
 
 Auth::routes();
 
