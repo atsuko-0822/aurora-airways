f696bc386038e20fcf8805637561553914d9943e
Kodai Sato <satkou115@gmail.com>
2025-05-02 20:10:24 +0900

    [UI/Homepage] - any change on template.blade.php - Added frontend of homepage-home.blade.php - Any change on homepage.css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,34 +3,34 @@
 @section('title', 'Homepage')
 
 @section('content')
-    <section class="hero position-relative text-center text-white">
-        <img src="/image/airplaneall.png" class="img-fluid w-100" style="max-block-size: 600px; object-fit: cover;" alt="Aurora Airways Airplane">
+    <section class="hero position-relative text-center">
+        <img src="/image/airplaneall.png" class="img-fluid w-100" alt="Aurora Airways Airplane">
         <div class="position-absolute top-50 start-50 translate-middle">
             <h1 class="display-4 mb-4">The finest experience for you</h1>
-            <button class="btn btn-success btn-lg">Book Now</button>
+            <button class="btn btn-lg">Book Now</button>
         </div>
     </section>
 
     <section class="promotions py-5">
         <div class="container">
-            <div class="row g-4">
+            <div class="row g-4 text-center">
                 <div class="col-md-6">
                     <div class="card bg-light rounded-4 overflow-hidden shadow-sm">
-                        <img src="/image/Aurora.jpg" class="card-img-top" style="max-height: 300px; object-fit: cover;" alt="Your venture for Aurora">
+                        <img src="/image/Aurora.jpg" class="card-img-top" alt="Your venture for Aurora">
                         <div class="card-body">
                             <h3 class="card-title">Your venture for Aurora</h3>
                             <p class="card-text text-muted">Explore the breathtaking Northern Lights.</p>
-                            <a href="#" class="btn btn-primary stretched-link">Learn more</a>
+                            <a href="#" class="btn stretched-link">Learn more</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="card bg-light rounded-4 overflow-hidden shadow-sm">
-                        <img src="/image/summer.jpg" class="card-img-top" style="max-block-size: 300px; object-fit: cover;" alt="Book your summer trip now">
+                        <img src="/image/summer.jpg" class="card-img-top" alt="Book your summer trip now">
                         <div class="card-body">
-                            <h3 class="card-title text-warning">Book your summer trip now</h3>
+                            <h3 class="card-title">Book your summer trip now</h3>
                             <p class="card-text text-muted">Find the best deals for your summer vacation.</p>
-                            <a href="#" class="btn btn-warning stretched-link">Book now</a>
+                            <a href="#" class="btn stretched-link">Book now</a>
                         </div>
                     </div>
                 </div>
@@ -44,7 +44,7 @@
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
                 <div class="col">
                     <div class="card bg-light rounded-4 overflow-hidden shadow-sm">
-                        <img src="/image/toronto.jpg" class="card-img-top" style="max-block-size: 200px; object-fit: cover;" alt="Toronto">
+                        <img src="/image/toronto.jpg" class="card-img-top" alt="Toronto">
                         <div class="card-body text-center">
                             <h5 class="card-title">Toronto</h5>
                             <a href="#" class="card-text text-muted small">Discover for yourself</a>
@@ -53,7 +53,7 @@
                 </div>
                 <div class="col">
                     <div class="card bg-light rounded-4 overflow-hidden shadow-sm">
-                        <img src="/image/vancouver.jpg" class="card-img-top" style="max-block-size: 200px; object-fit: cover;" alt="Vancouver">
+                        <img src="/image/vancouver2.jpg" class="card-img-top" alt="Vancouver">
                         <div class="card-body text-center">
                             <h5 class="card-title">Vancouver</h5>
                             <a href="#" class="card-text text-muted small">Discover for yourself</a>
@@ -62,7 +62,7 @@
                 </div>
                 <div class="col">
                     <div class="card bg-light rounded-4 overflow-hidden shadow-sm">
-                        <img src="/image/ottawa.jpg" class="card-img-top" style="max-block-size: 200px; object-fit: cover;" alt="Ottawa">
+                        <img src="/image/ottawa.jpg" class="card-img-top" alt="Ottawa">
                         <div class="card-body text-center">
                             <h5 class="card-title">Ottawa</h5>
                             <a href="#" class="card-text text-muted small">Discover for yourself</a>
@@ -71,7 +71,7 @@
                 </div>
                 <div class="col">
                     <div class="card bg-light rounded-4 overflow-hidden shadow-sm">
-                        <img src="/image/montreal.jpg" class="card-img-top" style="max-block-size: 200px; object-fit: cover;" alt="Montreal">
+                        <img src="/image/montreal.jpg" class="card-img-top" alt="Montreal">
                         <div class="card-body text-center">
                             <h5 class="card-title">Montreal</h5>
                             <a href="#" class="card-text text-muted small">Discover for yourself</a>
@@ -85,5 +85,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDxyJejXiSWCw5LPg9mY1bRYYyI" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 @endsection
-
-
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -26,27 +26,27 @@
                 <a class="navbar-brand" href="/"> <img src="/image/logo.jpeg" alt="Aurora Airways Logo" class="logo-img" style="max-height: 100%; height: 50px;">
                 </a>
 
-                <ul class="navbar-nav mx-auto d-flex flex-row" style="color: #0F384A;">
+                <ul class="navbar-nav mx-auto d-flex flex-row">
                     <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: #0F384A;">Home</a>
+                        <a class="nav-link" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: #0F384A;">My bookings</a>
+                        <a class="nav-link" href="#">My bookings</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: #0F384A;">Check in</a>
+                        <a class="nav-link" href="#">Check in</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: #0F384A;">Flights</a>
+                        <a class="nav-link" href="#">Flights</a>
                     </li>
                 </ul>
 
-                <div class="d-flex align-items-center " style="color: #3E8BAC;">
-                    <i class="fa-solid fa-magnifying-glass" style="font-size: 1.7em;"></i>
-                    <i class="fa-solid fa-globe" style="font-size: 1.7em;"></i>
-                    <i class="fa-solid fa-bell" style="font-size: 1.7em;"></i>
+                <div class="d-flex align-items-center ">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <i class="fa-solid fa-globe"></i>
+                    <i class="fa-solid fa-bell"></i>
 
-                    <button class="btn btn-outline-primary" style="color: #3E8BAC; border-color: #3E8BAC; font-size: 1.1em; padding: 0.3em 0.7em;">Login</button>
+                    <button class="btn btn-outline-primary">Login</button>
                 </div>
             </div>
         </nav>
@@ -55,46 +55,46 @@
         </main>
 
 
-        <footer class="text-light py-4" style="color: #0F384A !important;">
+        <footer class="py-4">
             <div class="container-fluid">
                 <div class="row row-cols-1 row-cols-md-4">
                     <div class="col">
                         <h4 class="pb-2 mb-3">Customer Service</h4>
                         <ul class="list-unstyled">
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">Contact Us</a></li>
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">FAQ</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">FAQ</a></li>
                         </ul>
                     </div>
                     <div class="col">
                         <h4 class="pb-2 mb-3">Inspiration</h4>
                         <ul class="list-unstyled">
-                            <li><a href="#" style="color: #0F384A; text-decoration: none;">Travel Guides</a></li>
-                            <li><a href="#" style="color: #0F384A; text-decoration: none;">Flight Schedules</a></li>
+                            <li><a href="#">Travel Guides</a></li>
+                            <li><a href="#">Flight Schedules</a></li>
                         </ul>
                     </div>
                     <div class="col">
                         <h4 class="pb-2 mb-3">About Us</h4>
                         <ul class="list-unstyled">
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">Our Story</a></li>
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">Careers</a></li>
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">Our Community</a></li>
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">Sustainability</a></li>
-                            <li><a href="#"  style="color: #0F384A; text-decoration: none;">Publicity</a></li>
+                            <li><a href="#">Our Story</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Our Community</a></li>
+                            <li><a href="#">Sustainability</a></li>
+                            <li><a href="#">Publicity</a></li>
                         </ul>
                     </div>
                     <div class="col">
                         <h4 class="pb-2 mb-3">Connect with us</h4>
-                        <div class="d-flex flex-column flex-md-row flex-wrap gap-3" style="max-width: 150px;">
-                            <a href="#" style="color: #0F384A; font-size: 2.0em; width: 25%; display: inline-block; text-align: center;"><i class="fa-solid fa-comment"></i></a>
-                            <a href="#" style="color: #0F384A; font-size: 2.0em; width: 25%; display: inline-block; text-align: center;"><i class="fa-brands fa-twitter"></i></a>
-                            <a href="#" style="color: #0F384A; font-size: 2.0em; width: 25%; display: inline-block; text-align: center;"><i class="fa-brands fa-facebook-f"></i></a>
-                            <a href="#" style="color: #0F384A; font-size: 2.0em; width: 25%; display: inline-block; text-align: center;"><i class="fa-brands fa-instagram"></i></a>
-                            <a href="#" style="color: #0F384A; font-size: 2.0em; width: 25%; display: inline-block; text-align: center;"><i class="fa-brands fa-youtube"></i></a>
-                            <a href="#" style="color: #0F384A; font-size: 2.0em; width: 25%; display: inline-block; text-align: center;"><i class="fa-brands fa-snapchat"></i></a>
+                        <div class="d-flex flex-column flex-md-row flex-wrap gap-3">
+                            <a href="#"><i class="fa-solid fa-comment"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                            <a href="#"><i class="fa-brands fa-snapchat"></i></a>
                         </div>
                     </div>
                 </div>
-                <div class="subfooter d-flex justify-content-between pt-3 mt-3" style="padding-left: 0; padding-right: 0; margin-left: -12px; margin-right: -12px; color: #0F384A !important;">
+                <div class="subfooter d-flex justify-content-between pt-3 mt-3">
                     <p class="small">Aurora Airways Canada Ltd.</p>
                     <p class="small">All rights reserved.</p>
                     <p class="small">&copy; 2025 Aurora Airways group</p>
