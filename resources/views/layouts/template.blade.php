70e9e59139fd3f82be2f5520df55cf86fbc212ed
Atsuko <atk.822@gmail.com>
2025-05-05 20:43:52 +0900

    [UI/edit_profile] - Added edit profile UI Form - Added edit_profile.blade.php - Added edit_profile.css

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,6 +22,7 @@
     <link rel="stylesheet" href="{{ asset('css/template.css') }}">
     <link rel="stylesheet" href="{{ asset('css/departure.css') }}">
     <link rel="stylesheet" href="{{ asset('css/user_dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/edit_profile.css') }}">
 
 
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
