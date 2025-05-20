561302b37d81fe2922342abf58625fe798789f6c
Atsuko <atk.822@gmail.com>
2025-05-20 21:04:41 +0900

    [UI/payment] - Added payment UI Form - Added payment.blade.php - Added payment.css

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -30,6 +30,7 @@
     <link rel="stylesheet" href="{{ asset('css/flight_comparison.css') }}">
     <link rel="stylesheet" href="{{ asset('css/notifications.css') }}">
     <link rel="stylesheet" href="{{ asset('css/selected_flight.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/payment.css') }}">
 
 
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
