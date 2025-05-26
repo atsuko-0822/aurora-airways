8033986a663a91c4c65ad7d7d6ccc5c0145f9855
Atsuko <atk.822@gmail.com>
2025-05-26 21:48:07 +0900

    [BE/manage_users] -add admin_login.js (extract js file from admin_login.blade.php)

--- resources/views/admin_login.blade.php
+++ resources/views/admin_login.blade.php
@@ -34,22 +34,7 @@
     </div>
   </div>
 
-   <script>
-    function togglePassword() {
-      const passwordInput = document.getElementById('password');
-      const toggleEye = document.getElementById('toggleEye');
-
-      if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        toggleEye.classList.remove('fa-eye');
-        toggleEye.classList.add('fa-eye-slash');
-      } else {
-        passwordInput.type = 'password';
-        toggleEye.classList.remove('fa-eye-slash');
-        toggleEye.classList.add('fa-eye');
-      }
-    }
-  </script>
+  <script src="{{ asset('js/admin_login.js') }}"></script>
 
   @endsection
 
